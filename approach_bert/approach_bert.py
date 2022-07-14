d0f24f17f1ea90b5938c60fc8b343152de40fb41
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2022-07-14 14:07:07 -0300

    upd: read and classify from csv input + comments

--- approach_bert/approach_bert.py
+++ approach_bert/approach_bert.py
@@ -6,14 +6,15 @@ from pprint import pprint
 import pandas as pd
 import numpy as np
 import re
+from tqdm import tqdm
 
-#Folder path containing the fine-tuned model files
+# Folder path containing the fine-tuned model files, link suggested above to download from google drive
 model_path = './BERT'
 
+# Set model, tokenizer and prepare classifier
 model = BertForSequenceClassification.from_pretrained(model_path)
 tokenizer = BertTokenizer.from_pretrained(model_path)
-
-classifier = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
+classifier = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True) #, top_k=1 )
 
 def clean_tweet(tweet):
     '''
@@ -22,31 +23,33 @@ def clean_tweet(tweet):
     '''
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
+# Reading csv from file with index already done
 df_train = pd.read_csv("../data/tweets_stock_clean.csv").set_index('tweet_id')
 df_test = pd.read_csv("../data/tweets_stocks-full_agreement.csv").set_index('tweet_id')
 
-targets = ['TRU', 'DIS', 'JOY', 'SAD', 'ANT', 'SUR', 'ANG', 'FEA']
-
-to_delete = ['NEUTRAL', 'conf_tru_dis', 'conf_joy_sad', 'conf_ant_sur',
+# Extra info as to what is useful and what is useless
+targets = ['TRU', 'DIS', 'JOY', 'SAD', 'ANT', 'SUR', 'ANG', 'FEA', 'NEUTRAL']
+to_delete = ['conf_tru_dis', 'conf_joy_sad', 'conf_ant_sur',
        'conf_ang_fea', 'num_annot']
 
+# Drop useless columns
 df_train.drop(columns=to_delete, inplace=True)
 df_test.drop(columns=to_delete, inplace=True)
 
+# Apply our clean_tweet function to all text
 df_train['text'] = df_train['text'].apply(lambda text: clean_tweet(text))
 df_test['text'] = df_test['text'].apply(lambda text: clean_tweet(text))
 
-inputs = [
-	'Eu te amo',
-	'Eu acho que você é uma ótima pessoa',
-	'Eu odeio aquele cara',
-	]
+inputs = []
 
-output = classifier(inputs)
+print("Collecting tweets and adding into inputs...")
+for tweet in tqdm(df_test['text']):
+    inputs.append(tweet)
+
+output = classifier(inputs) # This is harder to read = list[list][dict, dict]
 
 predictions = []
 
-for prediction in output:
+print("Transferring output into a predictions list...")
+for prediction in tqdm(output): # Make it easier to read = list[dict]['str', 'float']
 	predictions.append(list(x for x in prediction))
-
-pprint(predictions)
